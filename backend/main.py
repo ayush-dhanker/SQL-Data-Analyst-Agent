ecb028718d41f4ef793c79d2e1af9c3511d3f542
ayush-dhanker <87539370+ayush-dhanker@users.noreply.github.com>
2026-04-27 01:06:05 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React dev server
+    allow_origins=[
+        "http://localhost:5173",
+        "https://sql-data-analyst-agent.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
